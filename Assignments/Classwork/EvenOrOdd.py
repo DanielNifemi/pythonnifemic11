f1b97cca17cfce793daeec8220ea431cadc80562
Nifemi Daniel <nifemidaniel50@gmail.com>
2022-06-02 11:27:36 -0700

    My first python program

---
+++ Assignments/Classwork/EvenOrOdd.py
@@ -0,0 +1,8 @@
+
+
+numbers = int(input("enter your number: "))
+
+if numbers % 2:
+    print("It is odd")
+else:
+    print("it is even")
---
+++ Assignments/PersonalProject/Response.py
@@ -0,0 +1,5 @@
+
+
+print("valentina:""Hello my name is valentina , what's your name!!: ")
+name = input()
+print("valentina: "" Hello " +name+ " Have a nice day")
